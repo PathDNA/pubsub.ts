4fb243766a955ba95f6ba64be0716d3b8861f76b
itsMontoya <josh@montoya.io>
2017-08-23 10:16:14 -0700

    Expanding commenting

--- common.ts
+++ common.ts
@@ -1,32 +1,47 @@
-
+// SubFn is called on entry update notification
 export type SubFn<T> = (key: string, val: T | null) => boolean;
 
+// NewSubMap will return a new subscription map
 export function NewSubMap<T>(): SubMap<T> {
 	const m = <SubMap<T>>{};
 	return m;
 }
 
+// SubMap is a QoL type for a common used object type
 export type SubMap<T> = { [key: string]: SubFn<T> };
 
+// Indexer manages an index state
 export class Indexer {
+	// Prefix value to prepend to generated indexes
 	private prefix: string;
+	// Current index value
 	private idx: number;
 
 	constructor(prefix: string) {
+		// Set prefix value as provided prefix
 		this.prefix = prefix;
+		// Set current index value to 0
 		this.idx = 0;
 	}
 
+	// Get will get the current index value (and increment for the next use).
 	Get(): string {
+		// Get current index as a string
 		const idx = this.idx.toString();
+		// Increment index
 		this.idx++;
+		// Return prefix concatinated with string index
 		return this.prefix + idx;
 	}
 }
 
+// Subber manages subscribing functions
 export class Subber<T> {
+	// Subscriber map
 	private m: SubMap<T>;
+	// Index manager
 	private i: Indexer;
+	// Closed state
 	private closed: boolean;
 
 	constructor(key: string) {
@@ -35,20 +50,22 @@ export class Subber<T> {
 		this.closed = false;
 	}
 
+	// Signal will notify all of the subscribers of the provided key/value pair
 	Signal(key: string, value: T | null) {
 		if (this.closed) {
 			return;
 		}
 
+		// Iterate through each subscriber
 		this.ForEach((skey: string, sfn: SubFn<T>) => {
-			if (sfn(key, value) !== true) {
-				return;
+			if (sfn(key, value) === true) {
+				// Subscribing function returned true, unsubscribe
+				this.Unsub(skey);
 			};
-
-			this.Unsub(skey);
 		});
 	}
 
+	// Sub will add the provided function to the subscribers list
 	Sub(fn: SubFn<T>, subKey?: string): string {
 		if (this.closed) {
 			return "";
@@ -66,6 +83,7 @@ export class Subber<T> {
 		return subKey;
 	}
 
+	// Unsub will remove the matching value to the provided key from the subscribers list
 	Unsub(key: string): boolean {
 		if (this.closed) {
 			return false;
@@ -75,14 +93,18 @@ export class Subber<T> {
 	}
 
 	ForEach(fn: (sKey: string, sfn: SubFn<T>) => void) {
+		// Iterate through each key of the map
 		Object.keys(this.m).forEach((subKey: string) => {
+			// Get subscription function
 			const sfn = this.m[subKey];
 			if (!sfn) {
+				// This should NEVER happen
+				console.error("Subscription function does not exist", subKey, this.m);
 				return;
 			}
 
+			// Pass the subkey and subscription function to the provided function
 			fn(subKey, sfn);
-			return;
 		});
 	}
 
@@ -92,6 +114,8 @@ export class Subber<T> {
 		}
 
 		this.closed = true;
+		// Replace map with a fresh map to wipe all the subscribers
+		// TODO: Investigate if this will leak, if so - manually de-reference to help the GC
 		this.m = NewSubMap();
 	}
 }
